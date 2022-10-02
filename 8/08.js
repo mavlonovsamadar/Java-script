49a9d494f106a86a0726d3c26cb07cc645156cf0
samandar92 <mavlonovsamandar1992@gmail.com>
2022-10-02 20:54:57 +0500

    08

---
+++ 8/08.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // getRate();
+    // getHistoryRate();
+    $('input[type="date"]').on('input', getHistoryRate);
+});
+
+function getHistoryRate() {
+    // https://api.coindesk.com/v1/bpi/currentprice.json
+    $.get(
+        "https://api.coindesk.com/v1/bpi/historical/close.json",
+        {
+            "start" : $('#date1').val(),
+            "end": $('#date2').val()
+        },
+        function (data) {
+             data = JSON.parse(data);
+            console.log(data);
+        }
+    );
+}
